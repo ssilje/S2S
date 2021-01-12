a970345aa765f7359d4afeda70f757ccb72cf580
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-12 16:43:54 +0100

    Update getdata_hindcast_CY46R1_ECMWF.py

--- getdata_hindcast_CY46R1_ECMWF.py
+++ getdata_hindcast_CY46R1_ECMWF.py
@@ -6,7 +6,7 @@ server = ECMWFDataServer()
 
 #datadir = '/cluster/work/users/sso102/S2S/hindcast/ECMWF/sfc/tp'
 dir = '/cluster/work/users/sso102/S2S/hindcast/ECMWF/sfc'
-
+forcastcycle = 'CY46R1'
 basedict = {
     'class': 's2',
     'dataset': 's2s',
@@ -17,16 +17,27 @@ basedict = {
     'time': '00:00:00'
 }
 
+l = range(0,1128,24)
+paired = ['-'.join([str(v) for v in l[i:i + 2]]) for i in range(len(l))]
+final = '/'.join(paired[0:-1])
+
 meta = {
     'tp': {
         'param': '228228',  
         'levtype': 'sfc',
         'grid': '1/1',
-        'step': '/'.join(['%i'%i for i in range(0,1104,24)]) # 40 days forecast
+        'step': '/'.join(['%i'%i for i in range(0,1104,24)]) 
+    },
+    
+     't2m': {
+        'param': '167',  
+        'levtype': 'sfc',
+        'grid': '1/1',
+        'step': '/'.join([final]) 
     }
 
 }
- 
+
 def getdatesformonth(month):
     a = (
        '2018-01-01',
@@ -40,7 +51,7 @@ def getdatesformonth(month):
    # Program start
 for filename in (
     'tp',
-    #'sst',
+    't2m',
 ):
     for month in range(1,13):
 
@@ -49,11 +60,11 @@ for filename in (
         for d in dates:
             refyear = int(d[:4])
             prefix = 'pf'
-            #datadir = '/cluster/work/users/sso102/S2S/hindcast/ECMWF/sfc/tp'
-            datadir = '%s/%s/_%s_%s_%s.grb'%(dir,filename)
+            datadir = '%s/%s'%(dir,filename)
+            if not os.path.exists(datadir)  :
+                os.makedirs(datadir)
             hdate = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
-            #target = '%s/%s_%s_%s_%s_%s.grb'%(datadir,filename,prefix,d,'hc', hdate)
-            target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,'CY46R1',d,prefix)
+            target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,forcastcycle,d,prefix)
 
             #tp_CY46R1_${d}_cf.grb
             if not os.path.isfile(target):
