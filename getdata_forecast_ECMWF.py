67e30c39805bdecec81c90c879884046a2eb3bfe
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-12 09:52:06 +0100

    Update getdata_forecast_ECMWF.py

--- getdata_forecast_ECMWF.py
+++ getdata_forecast_ECMWF.py
@@ -45,7 +45,7 @@ for filename in (
         dates = getdatesformonth(month)
 
         for d in dates:
-            prefix = 'cf'
+            prefix = 'pf'
             target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,'CY46R1',prefix,d)
             #target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,'CY46R1',d,prefix)    
             
@@ -56,6 +56,8 @@ for filename in (
                 dic['date'] = d
                 dic['type'] = prefix
                 dic['target'] = target
+                if prefix == 'pf':
+                    dic['number'] = '1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50'
                 print(dic)
                 if server is not None:
                     server.retrieve(dic)
