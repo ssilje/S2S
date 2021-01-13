820a7f99d337ab66558ed9635ff909d7166182db
Silje Sørland <silje.soerland@env.ethz.ch>
2021-01-13 15:40:14 +0100

    Update getdata_forecast_t2m_pr_ECMWF.py

--- getdata_forecast_t2m_pr_ECMWF.py
+++ getdata_forecast_t2m_pr_ECMWF.py
@@ -1,1 +1,81 @@
 
+#!/usr/bin/env python
+from ecmwfapi import ECMWFDataServer
+import os,sys
+from datetime import datetime
+server = ECMWFDataServer()
+#datadir = '/cluster/work/users/sso102/S2S/forecast/ECMWF/sfc/tp'
+dir = '/cluster/work/users/sso102/S2S/hindcast/ECMWF/sfc'
+forcastcycle = 'CY46R1'
+basedict = {
+    'class': 's2',
+    'dataset': 's2s',
+    'expver': 'prod',
+    'model': 'glob',
+    'origin': 'ecmf',
+    'stream': 'enfo', #changed from enfh
+    'time': '00:00:00'
+}
+
+l = range(0,1128,24)
+paired = ['-'.join([str(v) for v in l[i:i + 2]]) for i in range(len(l))]
+final = '/'.join(paired[0:-1])
+
+meta = {
+    'tp': {
+        'param': '228228',  
+        'levtype': 'sfc',
+        'grid': '1/1',
+        'step': '/'.join(['%i'%i for i in range(0,1104,24)]) # 40 days forecast
+    },
+  
+    't2m': {
+        'param': '167',  
+        'levtype': 'sfc',
+        'grid': '1/1',
+        'step': '/'.join([final]) 
+    }
+
+
+}
+ 
+def getdatesformonth(month):
+    a = (
+       '2018-01-01',
+    )
+    for b in a:
+        dates = b.split('/')
+        if datetime.strptime(dates[0],'%Y-%m-%d').month == month:
+            return dates
+    return []   
+    
+   # Program start
+for filename in (
+    #'tp',
+    't2m',
+):
+    for month in range(1,13):
+
+        dates = getdatesformonth(month)
+
+        for d in dates:
+            prefix = 'pf'
+            datadir = '%s/%s'%(dir,filename)
+            if not os.path.exists(datadir)  :
+                os.makedirs(datadir
+            target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,forcastcycle,prefix,d)
+           # target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,'CY46R1',prefix,d)
+            #target = '%s/%s_%s_%s_%s.grb'%(datadir,filename,'CY46R1',d,prefix)    
+            
+            if not os.path.isfile(target):
+                dic = basedict.copy()
+                for k,v in meta[filename].items():
+                    dic[k] = v
+                dic['date'] = d
+                dic['type'] = prefix
+                dic['target'] = target
+                if prefix == 'pf':
+                    dic['number'] = '1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50'
+                print(dic)
+                if server is not None:
+                    server.retrieve(dic)
