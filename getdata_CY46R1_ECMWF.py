86c6e2808dde7dbb365b45923817e620a681299e
Silje Sørland <silje.soerland@env.ethz.ch>
2021-02-10 16:41:45 +0100

    Update getdata_CY46R1_ECMWF.py

--- getdata_CY46R1_ECMWF.py
+++ getdata_CY46R1_ECMWF.py
@@ -59,55 +59,41 @@ meta = {
     }
 }
 
-def getdatesformonth(month):
-    a = (
-       '2018-01-01',
-    )
-    for b in a:
-        dates = b.split('/')
-        if datetime.strptime(dates[0],'%Y-%m-%d').month == month:
-            return dates
-    return []   
+dates_monday = pd.date_range("20190701", periods=52, freq="7D") # forecats start Monday
+dates_thursday = pd.date_range("20190704", periods=52, freq="7D") # forecats start Thursday
+   
     
    # Program start
 for filename in (
     'VAR',
 ):
-    for month in range(1,13):
-
-        dates = getdatesformonth(month)
-
-        for d in dates:
-            refyear = int(d[:4])
-            prefix = 'ftype'
-            yy = 0
-            i =refyear-yy
-            datadir = '%s/%s'%(dir,filename)
-            if not os.path.exists(datadir)  :
-                os.makedirs(datadir)
-            hdate = '/'.join([d.replace('%i'%refyear,'%i'%i)])
-            #hdate = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
-            target = '%s/%s_%s_%s_%s_%s.grb'%(datadir,filename,forcastcycle,d,prefix,hdate)
-        
-            if not os.path.isfile(target):
-                dic = basedict.copy()
-                for k,v in meta[filename].items():
-                    dic[k] = v
-                dic['date'] = d
-                dic['type'] = prefix
-                if ( product == 'hindcast' ):
-                    dic['hdate'] = hdate
-                    if prefix == 'pf':
-                        ll = range(1,11,1) # 10 members
-                        paired = '/'.join(['/'.join([str(v) for v in ll[i:i + 1]]) for i in range(len(ll))])
-                        dic['number'] =  '%s' %(paired)
-                if ( product == 'forecast' ):
-                    if prefix == 'pf':
-                        ll = range(1,51,1) # 50 members
-                        paired = '/'.join(['/'.join([str(v) for v in ll[i:i + 1]]) for i in range(len(ll))])
-                        dic['number'] =  '%s' %(paired)
-                dic['target'] = target
-                
-                print(dic)
-                if server is not None:
-                    server.retrieve(dic)
+    for dates in dates_monday:
+        d = dates.strftime('%Y-%m-%d')
+        refyear = int(d[:4])
+        prefix = 'ftype'
+        datadir = '%s/%s'%(dir,filename)
+        if not os.path.exists(datadir)  :
+            os.makedirs(datadir)
+         hdate = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
+         target = '%s/%s_%s_%s_%s_%s.grb'%(datadir,filename,forcastcycle,d,prefix,hdate)
+         if not os.path.isfile(target):
+            dic = basedict.copy()
+            for k,v in meta[filename].items():
+                dic[k] = v
+            dic['date'] = d
+            dic['type'] = prefix
+            if ( product == 'hindcast' ):
+                dic['hdate'] = hdate
+                if prefix == 'pf':
+                    ll = range(1,11,1) # 10 members
+                    paired = '/'.join(['/'.join([str(v) for v in ll[i:i + 1]]) for i in range(len(ll))])
+                    dic['number'] =  '%s' %(paired)
+            if ( product == 'forecast' ):
+                if prefix == 'pf':
+                    ll = range(1,51,1) # 50 members
+                    paired = '/'.join(['/'.join([str(v) for v in ll[i:i + 1]]) for i in range(len(ll))])
+                    dic['number'] =  '%s' %(paired)
+            dic['target'] = target    
+            print(dic)
+            if server is not None:
+                server.retrieve(dic)
